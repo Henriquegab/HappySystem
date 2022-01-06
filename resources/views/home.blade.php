30215cc9e98380e86f1f297aca9f2b239f730e94
Henrique Gabriel Siqueira da Cruz <henriquepro8@gmail.com>
2022-01-06 15:59:54 -0300

    Widget meta de cliente

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,20 +15,55 @@
     </x-adminlte-alert>
     @endif
 
+    @php
+        use App\Models\Cliente;
+        use App\Models\Produto;
+        use App\Models\Pedido;
+
+        $metaCliente = 300;
+        $progresso = Cliente::count() / $metaCliente * 100;
+    @endphp
+
     <div class="row">
 
 
-        <div class="col-md-8">
-            <x-adminlte-info-box title="Tasks" text="75/100" icon="fas fa-lg fa-tasks text-orange" theme="warning"
-            icon-theme="dark" progress=50 progress-theme="dark"
-            description="75% of the tasks have been completed"/>
+        <div class="col-md-6">
+            <x-adminlte-info-box title="Meta de Clientes" text="{{ Cliente::count() }}/{{ $metaCliente }}" icon="fas fa-lg fa-users text-orange" theme="warning"
+            icon-theme="dark" progress="{{ intval($progresso)  }}" progress-theme="dark"
+            description="{{ intval($progresso)  }}% da meta concluida!"/>
+        </div>
+
+
+        <div class="col-md-6">
+            <x-adminlte-info-box title="Reputation" text="0/1000" icon="fas fa-lg fa-medal text-dark"
+            theme="danger" id="ibUpdatable" progress=0 progress-theme="teal"
+            description="0% reputation completed to reach next level"/>
+
+            
+        </div>
+
+       
+    </div>
+    <div class="row">
+
+        <div class="col-md-4">
 
             <x-adminlte-info-box title="Views" text="424" icon="fas fa-lg fa-eye text-dark" theme="gradient-teal"/>
         </div>
         <div class="col-md-4">
+        
             <x-adminlte-info-box title="Views" text="424" icon="fas fa-lg fa-eye text-dark" theme="gradient-teal"/>
 
+
         </div>
+
+        <div class="col-md-4">
+            <x-adminlte-info-box title="528" text="User Registrations" icon="fas fa-lg fa-user-plus text-primary"
+            theme="gradient-primary" icon-theme="white"/>
+
+            
+        </div>
+
     </div>
 @stop
 
