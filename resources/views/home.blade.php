ae7690fcc0cd6f382d1bd6fa63cc0f8b214ed0ab
Henrique Gabriel Siqueira da Cruz <henriquepro8@gmail.com>
2022-01-07 16:39:50 -0300

    implantação do chartJs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
 @stop
 
 @section('content')
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <p align="center">Seja Bem-vindo!</p>
     @if (isset($notification))
 
@@ -79,6 +80,61 @@
         </div>
 
     </div>
+
+    <x-adminlte-card title="Purple Card" theme="purple" icon="fas fa-lg fa-fan" removable collapsible >
+        
+       
+            <canvas id="myChart" width="200" height="50"></canvas>
+                <script>
+                    const ctx = document.getElementById('myChart').getContext('2d');
+                    const myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: ['Clientes', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                            datasets: [{
+                                label: '# of Votes',
+                                data: [<?php echo Cliente::count() ?>, 240, 3, 5, 2, 3],
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(255, 159, 64, 0.2)'
+                                ],
+                                borderColor: [
+                                    'rgba(255, 99, 132, 1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(153, 102, 255, 1)',
+                                    'rgba(255, 159, 64, 1)'
+                                ],
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    });
+                </script>
+        
+
+
+    </x-adminlte-card>
+
+    
+
+
+
+
+
+
+
 @stop
 
 @section('css')
